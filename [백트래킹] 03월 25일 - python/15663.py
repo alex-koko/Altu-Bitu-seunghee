bd58e7fc7172b588d1b357f3a310938c737bc522
alex-koko <86093028+alex-koko@users.noreply.github.com>
2022-03-31 03:13:04 +0900

    [백트래킹] 03월 25일 - 2문제 제출

---
+++ [백트래킹] 03월 25일 - python/15663.py
@@ -0,0 +1,14 @@
+﻿# 15663번
+import sys
+from itertools import combinations # 조합을 이용해서 문제를 해결할 것
+input = sys.stdin.readline
+
+
+# 입력
+n, m = input().split() # n, m을 입력받음
+numli = set(input().split()) # n개의 수가 주어지면 그 수를 집합(set)에 저장
+
+
+# 연산 + 출력
+a = list(combinations(numli, m)) # 집합 중에 m개의 원소를 선택해서 저장한 것의 리스트 생성
+print(list(a)) # 리스트 출력
